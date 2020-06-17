5163d7426a0af61c53f99dd481d50c7ea6bb6054
20180488 <pks5294@kumoh.ac.kr>
2020-06-17 14:30:37 +0900

    Second

---
+++ app.js
@@ -0,0 +1,20 @@
+const canvas = document.getElementById("jsCanvas");
+
+let painting = false;
+
+if (canvas) {
+  canvas.addEventListener("mousemove", (event) => {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log(x, y);
+  });
+  canvas.addEventListener("mousedown", (event) => {
+    painting = true;
+  });
+  canvas.addEventListener("mouseup", (event) => {
+    painting = false;
+  });
+  canvas.addEventListener("mouseleave", (event) => {
+    painting = false;
+  });
+}
